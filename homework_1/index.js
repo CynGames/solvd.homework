d7520107fc208f5d7d5347ab901c3ae226922791
CynGames <tomasm.leguizamon@gmail.com>
2023-01-24 15:11:44 -0300

    BigInt fix

--- homework_1/index.js
+++ homework_1/index.js
@@ -1,34 +1,30 @@
 // Homework 1 for Solvd IT Academy.
 
 String.prototype.plus = function (stringNumber) {
-    const result = parseInt(this) + parseInt(stringNumber);
-
+    const result = BigInt(this) + BigInt(stringNumber);
     return result.toString();
 }
 
 String.prototype.minus = function (stringNumber) {
-    const result = parseInt(this) - parseInt(stringNumber);
-
+    const result = BigInt(this) - BigInt(stringNumber);
     return result.toString();
 }
 
 String.prototype.divide = function (stringNumber) {
-    const result = parseInt(this) / parseInt(stringNumber);
-
+    const result = BigInt(this) / BigInt(stringNumber);
     return result.toString();
 }
-
 
 String.prototype.multiply = function (stringNumber) {
-    const result = parseInt(this) * parseInt(stringNumber);
-
+    const result = BigInt(this) * BigInt(stringNumber);
     return result.toString();
 }
 
-const plusResult = "123".plus("1");
-const minusResult = "123".minus("1");
-const divideResult = "100".divide("2");
-const multiplyResult = "100".multiply("2");
+
+const plusResult = "100000000000000".plus("9453495394594395");
+const minusResult = "100000000000000".minus("9453495394594395");
+const divideResult = "100000000000000".divide("9453495394594395");
+const multiplyResult = "100000000000000".multiply("9453495394594395");
 
 console.log(`Plus Result: ${plusResult}. The type of the result is ${typeof plusResult}`);
 console.log(`Minus Result: ${minusResult}. The type of the result is ${typeof minusResult}`);
