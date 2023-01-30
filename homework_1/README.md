1b0e6aeebe40a6c8982e8c9c3ac147c639eed41d
CynGames <tomasm.leguizamon@gmail.com>
2023-01-30 03:04:20 -0300

    Added multiplication and division

--- homework_1/README.md
+++ homework_1/README.md
@@ -12,38 +12,14 @@ String.multiple(string) => string
 ```
 
 ## Solution Process ##
-There are several ways to process large numbers without the explicit usage of BigInt, such as: 
-- Array Representation
-- Modular Arithmetic
-- Elementary School Algorithm
+There are mainly two ways to process large numbers without the explicit usage of BigInt. 
+- One is to use Array Representation. Which, as the name implies, is to represent the number as an array of digits. 
+- The other way is to mimic how we would perform arithmetic operations on two numbers on paper. 
 
-The most convenient way to solve this task is to implement the Elementary School Algorithm 
-due to the simplicity of the algorithm and the requirement to take strings as input.
-
-### General overview of the Elementary School Algorithm: ###
-The idea of the algorithm is to mimic how we would perform arithmetic operations on two numbers on paper.
-
-First, the code defines a new function, that is added to the prototype of the JavaScript String object. 
-This function can then be called on any string value and takes one argument, "num_as_string", 
-which is expected to be a number but as a string data type. 
-
-The function starts by initializing some variables: 
-- "firstNumber" is the string on which the function is called (i.e. "this").
-- "secondNumber" is the "num_as_string" argument.
-- "result" is an empty string that will be used to store the final result of the operation.
-- "carry" is an integer that will be used to track any carrying over of digits from one column of the addition to the next.
-
-Then, the two numbers are then padded with leading zeroes so that they have the same number of digits.
-This is to ensure that the digits align correctly for the addition.
-
-The for loop then starts at the rightmost digit of the two numbers and works its way to the left,
-adding the corresponding digits of the two numbers together, along with any carry from the previous column.
-The result of the addition is then divided by 10, and the remainder is appended to the "result" string,
-while the quotient is stored as the new value of "carry."
-
-Finally, if there is any remaining carry after all the digits have been processed, it is added to the front of the "result" string.
-The final value of the "result" string is then returned as the result of the function.
+We will be using the second method as it is more intuitive and easier to implement.
 
 ### Notes ###
 - Bitwise operators can be used to enhance the performance of the algorithm, but they make the code much less readable.
 - The String.prototype.minus() function does not support negative numbers.
+- The String.prototype.multiply() has precision issues regarding zeroes if the number is large.
+- The String.prototype.divide() function does not support decimal division.
--- homework_1/index.js
+++ homework_1/index.js
@@ -1,34 +1,17 @@
 // Homework 1 for Solvd IT Academy.
 // Long Arithmetic Algorithm.
 
-function CheckForNaN(firstNumber, secondNumber) {
-    if (isNaN(firstNumber) || isNaN(secondNumber)) {
-        throw new Error("The input parameters are not numbers.");
-    }
-}
-
 // The function takes two numbers as strings and returns their sum as a string.
 String.prototype.plus = function (numberToAdd) {
 
-    // Cache the input numbers to more readable variables
-    let firstNumber = this;
-    let secondNumber = numberToAdd;
+    // Cache and initialize input numbers to more readable variables
+    let {firstNumber, secondNumber, result, carry} = InitializeVariables(this, numberToAdd, "");
 
     // Do a NaN check in case the input parameters are not numbers
     CheckForNaN(firstNumber, secondNumber);
 
-    // Initialize the result and carry variables
-    let result = ""
-    let carry = 0;
-
     // Make sure both numbers have the same number of digits
-    while (firstNumber.length < secondNumber.length) {
-        firstNumber = "0" + firstNumber;
-    }
-
-    while (secondNumber.length < firstNumber.length) {
-        secondNumber = "0" + secondNumber;
-    }
+    ({firstNumber, secondNumber} = EqualizeNumberOfDigits(firstNumber, secondNumber));
 
     // Iterate through each digit of both numbers, starting from the rightmost digit
     for (let digitIndex = firstNumber.length - 1; digitIndex >= 0; digitIndex--) {
@@ -51,29 +34,19 @@ String.prototype.plus = function (numberToAdd) {
     }
 
     return result;
-}
+};
 
 // The function takes two numbers as strings and returns their difference as a string.
 String.prototype.minus = function (numberToSubtract) {
-    // Cache the input numbers to more readable variables
-    let firstNumber = this;
-    let secondNumber = numberToSubtract;
+
+    // Cache and initialize input numbers to more readable variables
+    let {firstNumber, secondNumber, result, carry} = InitializeVariables(this, numberToSubtract, "");
 
     // Do a NaN check in case the input parameters are not numbers
     CheckForNaN(firstNumber, secondNumber);
 
-    // Initialize the result and carry variables
-    let result = ""
-    let carry = 0;
-
     // Make sure both numbers have the same number of digits
-    while (firstNumber.length < secondNumber.length) {
-        firstNumber = "0" + firstNumber;
-    }
-
-    while (secondNumber.length < firstNumber.length) {
-        secondNumber = "0" + secondNumber;
-    }
+    ({firstNumber, secondNumber} = EqualizeNumberOfDigits(firstNumber, secondNumber));
 
     // Iterate through each digit of both numbers, starting from the rightmost digit
     for (let digitIndex = firstNumber.length - 1; digitIndex >= 0; digitIndex--) {
@@ -96,26 +69,120 @@ String.prototype.minus = function (numberToSubtract) {
     }
 
     // Remove leading zeros
+    result = TrimLeadingZeroes(result);
+
+    return result;
+};
+
+// The function takes two numbers as strings and returns their product as a string.
+String.prototype.multiply = function (numberToMultiply) {
+
+    // Initialize the result and carry variables.
+    // It is ensured that the result array will have enough space to store the maximum possible number of digits.
+    let {firstNumber, secondNumber, result, carry} = InitializeVariables(
+        this, numberToMultiply, Array(this.length + numberToMultiply.length).fill(0)
+    );
+
+    // Do a NaN check in case the input parameters are not numbers
+    CheckForNaN(firstNumber, secondNumber);
+
+    // Iterate through each digit of both numbers, starting from the rightmost digit
+    for (let i = firstNumber.length - 1; i >= 0; i--) {
+        for (let j = secondNumber.length - 1; j >= 0; j--) {
+            let product = result[i + j + 1] + (firstNumber[i] - '0') * (secondNumber[j] - '0');
+            carry = Math.floor(product / 10);
+            result[i + j + 1] = product % 10;
+            result[i + j] += carry;
+        }
+    }
+
+    // Remove leading zeros from the result
+    while (result.length > 1 && result[0] === 0) {
+        result.shift();
+    }
+
+    // Convert the result array to a string
+    result = result.join('');
+
+    return result;
+};
+
+// The function takes two numbers as strings and returns their quotient as a string.
+String.prototype.divide = function (numberToDivideBy) {
+
+    // Convert input strings to numbers
+    let dividend = this.toString();
+    let divisor = numberToDivideBy.toString();
+
+    // Check for NaN inputs
+    CheckForNaN(dividend, divisor);
+
+    // Check if divisor is equal to zero and return an error if it is
+    if (divisor === "0") {
+        return "Zero division error";
+    }
+
+    // Check if dividend is smaller than divisor, return '0' if it is
+    if (dividend < divisor) {
+        return "0";
+    }
+
+    // Initialize variables for storing the result and current value
+    let result = "";
+    let currentValue = "";
+
+    // Iterate through each digit of the dividend
+    for (let dividendIndex = 0; dividendIndex < dividend.length; dividendIndex++) {
+        currentValue += dividend[dividendIndex];
+        let quotient = 0;
+
+        // Determine the largest quotient that can be achieved
+        while (currentValue >= divisor) {
+            currentValue = (currentValue - divisor).toString();
+            quotient++;
+        }
+
+        // Add the quotient digit to the result
+        result += quotient;
+
+        // If we reach the end of the dividend and current value is not equal to zero, add a decimal point
+        if (dividendIndex === dividend.length - 1 && currentValue !== "0") {
+            result += ".";
+        }
+    }
+
+    // Trim leading zeros from the result
+    result = TrimLeadingZeroes(result);
+
+    return result;
+};
+
+function CheckForNaN(firstNumber, secondNumber) {
+    if (isNaN(firstNumber) || isNaN(secondNumber)) {
+        throw new Error("The input parameters are not numbers.");
+    }
+}
+
+function InitializeVariables(firstNumber, secondNumber, result, carry = 0) {
+    return {firstNumber, secondNumber, result, carry};
+}
+
+function EqualizeNumberOfDigits(firstNumber, secondNumber) {
+    while (firstNumber.length < secondNumber.length) {
+        firstNumber = "0" + firstNumber;
+    }
+
+    while (secondNumber.length < firstNumber.length) {
+        secondNumber = "0" + secondNumber;
+    }
+
+    return {firstNumber, secondNumber};
+}
+
+function TrimLeadingZeroes(result) {
     while (result[0] === '0') {
         result = result.substring(1);
     }
 
     return result;
 }
-
-String.prototype.divide = function (stringNumber) {
-    const result = BigInt(this) / BigInt(stringNumber);
-    return result.toString();
-}
-
-String.prototype.multiply = function (stringNumber) {
-    const result = BigInt(this) * BigInt(stringNumber);
-    return result.toString();
-}
-
-
-const divideResult = "100000000000000".divide("9453495394594395");
-const multiplyResult = "100000000000000".multiply("9453495394594395");
-
-console.log(`Divide Result: ${divideResult}`);
-console.log(`Multiply Result: ${multiplyResult}`);
